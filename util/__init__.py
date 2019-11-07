c8d9739403b6641037521d76af047d5e56187597
minjianxu <minjianxu@creditease.cn>
2019-11-07 15:42:00 +0800

    up

---
+++ util/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2019/11/6 4:12 下午 
+# @Author : minjianxu
+# @File : __init__.py.py 
---
+++ util/test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2019/11/6 4:40 下午 
+# @Author : minjianxu
+# @File : test.py
+
+import  numpy as np
+
+print(np.arange(1965,1995))
+
+print(np.arange(1,13))
+
+# print(day_arr)
+
+for i in range(100):
+    print(i)
+
+for a in range(1,10,2):
+    # print(a)
+    # print(np.random.randint(12))
+    # print(np.random.choice(['10','20','30']))
+    print(str(np.random.randint(100,999)))
